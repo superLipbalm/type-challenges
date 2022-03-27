2b1ccc584e4221100f7220caf4b6db49a4b6eee1
KIM CHAN HEE <superlipbalm@gmail.com>
2022-03-28 01:01:08 +0900

    Solve medium 119-replace-all

---
+++ medium/119-replace-all.ts
@@ -0,0 +1,5 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer A}${From}${infer B}`
+  ? `${A}${To}${ReplaceAll<B, From, To>}`
+  : S;
